d50bf2eeaa17a93e52720d10d1ffb0f5745ee93c
thuan.dh <thuan14121999@gmail.com>
2020-03-29 15:32:25 +0700

    Updates

---
+++ precache-manifest.cf8fb4ac38ddff3cd7a5f337b06815ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc2dc5e1a90eb041cd697fb0e289033",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "0db51a2565f9814bb34a",
+    "url": "/monsters/static/css/main.35703590.chunk.css"
+  },
+  {
+    "revision": "4f2d8a995868bd5310b9",
+    "url": "/monsters/static/js/2.b63b7e1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b63b7e1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db51a2565f9814bb34a",
+    "url": "/monsters/static/js/main.29e99505.chunk.js"
+  },
+  {
+    "revision": "48a948fffa185d281f13",
+    "url": "/monsters/static/js/runtime-main.23400953.js"
+  }
+]);
